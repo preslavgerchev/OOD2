0b99ff1669c81f5f743de6c4e8faafd5621631c3
Preslav <preslavgerchevmail@gmail.com>
2015-12-03 17:34:25 +0100

    Add 2nd project.
    Just the start of the project.

---
+++ OOD2WPF/OOD2WPF/Components/Merger.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace OOD2WPF
+{
+    class Merger : Component
+    {
+        public int UpperFlow { get; private set; } = 0;
+        public int /*Downer*/ LowerFlow { get; private set; } = 0;
+        private const int DefaultCurrentFlow = 0;
+
+        public Merger(int locX, int locY, int maxFlow)
+            : base(locX, locY, DefaultCurrentFlow, maxFlow)
+        {
+
+        }
+
+        public override Image GetImage()
+        {
+            return Properties.Resources.pump;
+            //needs to be changed to a proper image of a merger.
+        }
+
+        public void SetUpperFlow(int upperFlow)
+        {
+            this.UpperFlow = upperFlow;
+        }
+
+        public void SetLowerFlow(int lowerFlow)
+        {
+            this.LowerFlow = lowerFlow;
+        }
+
+        public int GetTotalFLow()
+        {
+            return this.LowerFlow + this.UpperFlow;
+        }
+    }
+}
---
+++ OOD2WPF/OOD2WPF/Components/Splitter.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Drawing;
+
+namespace OOD2WPF
+{
+    public class Splitter : Component
+    {
+        public int UpperFlow { get; private set; } = 0;
+        public int LowerFlow { get; private set; } = 0;
+
+        public Splitter(int locx, int locy, int currFlow, int maxFlow) :
+            base(locx, locy, currFlow, maxFlow)
+        {
+
+        }
+
+        public override Image GetImage()
+        {
+            return Properties.Resources.pump;
+            //needs to be changed to a proper image of a splitter.
+        }
+
+        public void SetUpperFlow(int upperFlow)
+        {
+            this.UpperFlow = upperFlow;
+        }
+
+        public void SetLowerFlow(int lowerFlow)
+        {
+            this.LowerFlow = lowerFlow;
+        }
+
+        public int GetCurrentFlow()
+        {
+            return this.LowerFlow + this.UpperFlow;
+        }
+    }
+}
